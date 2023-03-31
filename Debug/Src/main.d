537ebfb1eb2cdef452438a03ca6a2379ecb491ac
thanhluannn2008 <nguyenthanhluan.54183@gmail.com>
2023-03-31 21:58:25 +0700

    Them file mơi

---
+++ Debug/Src/main.d
@@ -0,0 +1,23 @@
+Src/main.o: ../Src/main.c \
+ D:/Lap\ Trinh/lap\ trinh/embedded/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/misc.h \
+ D:/Lap\ Trinh/lap\ trinh/embedded/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/stm32f401re.h \
+ D:/Lap\ Trinh/lap\ trinh/embedded/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cm4.h \
+ D:/Lap\ Trinh/lap\ trinh/embedded/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmInstr.h \
+ D:/Lap\ Trinh/lap\ trinh/embedded/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmFunc.h \
+ D:/Lap\ Trinh/lap\ trinh/embedded/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmSimd.h \
+ D:/Lap\ Trinh/lap\ trinh/embedded/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h \
+ D:/Lap\ Trinh/lap\ trinh/embedded/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_rcc.h \
+ D:/Lap\ Trinh/lap\ trinh/embedded/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_gpio.h \
+ D:/Lap\ Trinh/lap\ trinh/embedded/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_exti.h \
+ D:/Lap\ Trinh/lap\ trinh/embedded/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_syscfg.h
+D:/Lap\ Trinh/lap\ trinh/embedded/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/misc.h:
+D:/Lap\ Trinh/lap\ trinh/embedded/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/stm32f401re.h:
+D:/Lap\ Trinh/lap\ trinh/embedded/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cm4.h:
+D:/Lap\ Trinh/lap\ trinh/embedded/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmInstr.h:
+D:/Lap\ Trinh/lap\ trinh/embedded/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmFunc.h:
+D:/Lap\ Trinh/lap\ trinh/embedded/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmSimd.h:
+D:/Lap\ Trinh/lap\ trinh/embedded/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h:
+D:/Lap\ Trinh/lap\ trinh/embedded/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_rcc.h:
+D:/Lap\ Trinh/lap\ trinh/embedded/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_gpio.h:
+D:/Lap\ Trinh/lap\ trinh/embedded/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_exti.h:
+D:/Lap\ Trinh/lap\ trinh/embedded/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_syscfg.h:
